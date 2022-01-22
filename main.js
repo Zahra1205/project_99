e79cf2e5732642d065185baa35cdefa8390aaf26
Zahra1205 <78679374+Zahra1205@users.noreply.github.com>
2022-01-22 02:19:53 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+var SpeechRecognition=window.webkitSpeechRecognition;
+var recognition=new SpeechRecognition();
+function start(){
+    document.getElementById("text-box").innerHTML="";
+    recognition.start();
+
+}
+recognition.onresult=function run (event){
+    console.log (event);
+    var content=event.results[0][0].transcript;
+    console.log (content);
+    document.getElementById("text-box").innerHTML=content;
+    speak();
+}
+function speak(){
+var synth=window.speechSynthesis;
+var speak_data=document.getElementById("text-box").value;
+var utterthis=new SpeechSynthesisUtterance(speak_data);
+synth.speak(utterthis);
+Webcam.attach(camera);
+}
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'png',
+    png_quality: 90
+ });
+  var camera=document.getElementById("camera");
